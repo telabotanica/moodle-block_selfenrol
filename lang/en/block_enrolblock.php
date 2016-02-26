2c328cb3608ace8463bb249b5f243ab1bf531404
Mathias Chouet <mathias@tela-botanica.org>
2016-02-26 14:53:09 +0100

    try a right naming for lang files (2)

--- lang/en/block_enrolblock.php
+++ lang/en/block_enrolblock.php
@@ -1,6 +1,6 @@
 <?php
 $string['enrolblock:addinstance'] = 'Add an Enrol block';
-$string['enrolblock_title'] = 'Enrol block';
+$string['block_enrolblock_title'] = 'Enrol block';
 $string['already_enrolled'] = "You are already enrolled in this course";
 $string['not_enrolled_yet'] = "You are not enrolled in this course yet";
 $string['enrol_now'] = "Enrol now";
