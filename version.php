d26185220b9d1489e2f3bdcc66f4bf767aa30149
Mathias Chouet <mathias@tela-botanica.org>
2016-02-29 12:30:21 +0100

    Improved version details. Updated version number

--- version.php
+++ version.php
@@ -1,7 +1,9 @@
 <?php
 $plugin->component = 'block_selfenrol';
-$plugin->version = 2016022912;
+$plugin->version = 2016022913;
 $plugin->requires = 2010112400;
+$plugin->maturity = MATURITY_BETA;
+$plugin->release = "0.1";
 $plugin->dependencies = array(
 	'enrol_self' => ANY_VERSION,
 );
