f36dfaeb20535931d091daf105d6a307a16a4458
Mathias Chouet <mathias@tela-botanica.org>
2016-02-29 13:22:28 +0100

    Now requires Moodle v2.8

--- version.php
+++ version.php
@@ -1,7 +1,7 @@
 <?php
 $plugin->component = 'block_selfenrol';
-$plugin->version = 2016022913;
-$plugin->requires = 2010112400;
+$plugin->version = 2016022914;
+$plugin->requires = 2014111000; // Moodle v2.8
 $plugin->maturity = MATURITY_BETA;
 $plugin->release = "0.1";
 $plugin->dependencies = array(
